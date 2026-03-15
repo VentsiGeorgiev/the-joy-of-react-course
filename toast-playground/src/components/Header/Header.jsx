0883ae67e8e2a29ad3003644e0b1a92886bb39ff
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-15 13:35:44 +0100

    add hero image

--- toast-playground/src/components/Header/Header.jsx
+++ toast-playground/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import style from "./Header.module.css";
+import toasterHero from "../../assets/toaster-hero.png";
 
 function Header() {
   return (
     <header className={style.header}>
       <h1>Toast Playground</h1>
+      <img src={toasterHero} alt="toaster image" className={style.heroImage} />
     </header>
   );
 }
