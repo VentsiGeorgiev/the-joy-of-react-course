75688389ae1245d840e587198bcff5505d7ffde9
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-15 13:07:11 +0100

    enhance header components

--- toast-playground/src/components/Header/Header.jsx
+++ toast-playground/src/components/Header/Header.jsx
@@ -1,4 +1,10 @@
+import style from "./Header.module.css";
+
 function Header() {
-  return <h1>Toast Playground</h1>;
+  return (
+    <header className={style.header}>
+      <h1>Toast Playground</h1>
+    </header>
+  );
 }
 export default Header;
