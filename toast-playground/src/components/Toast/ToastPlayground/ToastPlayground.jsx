ef48299f884c5649edce82bfcc3557d4f7876aeb
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-15 14:28:44 +0100

    wrap toast provide within toast message

--- toast-playground/src/components/Toast/ToastPlayground/ToastPlayground.jsx
+++ toast-playground/src/components/Toast/ToastPlayground/ToastPlayground.jsx
@@ -1,10 +1,13 @@
 import { ToastForm } from "../ToastForm";
+import { ToasterProvider } from "../context/ToasterContext";
+import { ToastMessage } from "../ToastMessage";
 
 function ToastPlayground() {
   return (
-    <>
+    <ToasterProvider>
+      <ToastMessage />
       <ToastForm />
-    </>
+    </ToasterProvider>
   );
 }
 export default ToastPlayground;
